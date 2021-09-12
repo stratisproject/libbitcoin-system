a82eec4d7ce0894639ac3cc11cb1fbc70a966399
Lazar Prijovic <madrazzl3@gmail.com>
2021-09-12 22:45:12 +0300

    Remove unneeded tasks from build script

--- builds/msvc/build/nuget_all.bat
+++ builds/msvc/build/nuget_all.bat
@@ -3,13 +3,13 @@ ECHO Downloading libbitcoin vs2017 dependencies from NuGet
 CALL nuget.exe install ..\vs2017\libbitcoin-system\packages.config
 CALL nuget.exe install ..\vs2017\libbitcoin-system-examples\packages.config
 CALL nuget.exe install ..\vs2017\libbitcoin-system-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2015 dependencies from NuGet
-CALL nuget.exe install ..\vs2015\libbitcoin-system\packages.config
-CALL nuget.exe install ..\vs2015\libbitcoin-system-examples\packages.config
-CALL nuget.exe install ..\vs2015\libbitcoin-system-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2013 dependencies from NuGet
-CALL nuget.exe install ..\vs2013\libbitcoin-system\packages.config
-CALL nuget.exe install ..\vs2013\libbitcoin-system-examples\packages.config
-CALL nuget.exe install ..\vs2013\libbitcoin-system-test\packages.config
+REM ECHO.
+REM ECHO Downloading libbitcoin vs2015 dependencies from NuGet
+REM CALL nuget.exe install ..\vs2015\libbitcoin-system\packages.config
+REM CALL nuget.exe install ..\vs2015\libbitcoin-system-examples\packages.config
+REM CALL nuget.exe install ..\vs2015\libbitcoin-system-test\packages.config
+REM ECHO.
+REM ECHO Downloading libbitcoin vs2013 dependencies from NuGet
+REM CALL nuget.exe install ..\vs2013\libbitcoin-system\packages.config
+REM CALL nuget.exe install ..\vs2013\libbitcoin-system-examples\packages.config
+REM CALL nuget.exe install ..\vs2013\libbitcoin-system-test\packages.config
