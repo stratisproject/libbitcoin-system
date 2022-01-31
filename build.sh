c449c0db2e344d53536ebc71803a55f0caaffe2e
Lazar Prijovic @madrazzl3 <madrazzl3@gmail.com>
2022-01-31 12:38:59 +0300

    Fix build script format

--- build.sh
+++ build.sh
@@ -18,9 +18,7 @@ BUILD_DIR="build-libbitcoin-system"
 BOOST_URL="http://downloads.sourceforge.net/project/boost/boost/1.77.0/boost_1_77_0.tar.bz2"
 BOOST_ARCHIVE="boost_1_77_0.tar.bz2"
 
-
-configure_options()
-{
+configure_options() {
     display_message "configure options:"
     for OPTION in "$@"; do
         if [[ $OPTION ]]; then
@@ -31,31 +29,26 @@ configure_options()
     ./configure "$@"
 }
 
-create_directory()
-{
+create_directory() {
     local DIRECTORY="$1"
 
     rm -rf "$DIRECTORY"
     mkdir -p "$DIRECTORY"
 }
 
-display_heading_message()
-{
+display_heading_message() {
     printf "\n********************** %s **********************\n" "$@"
 }
 
-display_message()
-{
+display_message() {
     printf "%s\n" "$@"
 }
 
-display_error()
-{
-    >&2 printf "%s\n" "$@"
+display_error() {
+    printf >&2 "%s\n" "$@"
 }
 
-initialize_git()
-{
+initialize_git() {
     display_heading_message "Initialize git"
 
     # Initialize git repository at the root of the current directory.
@@ -64,8 +57,7 @@ initialize_git()
 }
 
 # make_current_directory jobs [configure_options]
-make_current_directory()
-{
+make_current_directory() {
     local JOBS=$1
     shift 1
 
@@ -76,8 +68,7 @@ make_current_directory()
 }
 
 # make_jobs jobs [make_options]
-make_jobs()
-{
+make_jobs() {
     local JOBS=$1
     shift 1
 
@@ -90,30 +81,25 @@ make_jobs()
     fi
 }
 
-pop_directory()
-{
+pop_directory() {
     popd >/dev/null
 }
 
-push_directory()
-{
+push_directory() {
     local DIRECTORY="$1"
 
     pushd "$DIRECTORY" >/dev/null
 }
 
-enable_exit_on_error()
-{
+enable_exit_on_error() {
     set -e
 }
 
-disable_exit_on_error()
-{
+disable_exit_on_error() {
     set +e
 }
 
-display_help()
-{
+display_help() {
     display_message "Usage: ./build.sh [OPTION]..."
     display_message "Manage the installation of libbitcoin-system."
     display_message "Script options:"
@@ -127,37 +113,33 @@ display_help()
 
 # Define environment initialization functions
 #==============================================================================
-parse_command_line_options()
-{
+parse_command_line_options() {
     for OPTION in "$@"; do
         case $OPTION in
-            # Standard script options.
-            (--help)                DISPLAY_HELP="yes";;
+        # Standard script options.
+        --help) DISPLAY_HELP="yes" ;;
 
             # Standard build options.
-            (--prefix=*)            PREFIX="${OPTION#*=}";;
+        --prefix=*) PREFIX="${OPTION#*=}" ;;
 
             # Unique script options.
-            (--build-dir=*)    BUILD_DIR="${OPTION#*=}";;
+        --build-dir=*) BUILD_DIR="${OPTION#*=}" ;;
         esac
     done
 }
 
-handle_help_line_option()
-{
+handle_help_line_option() {
     if [[ $DISPLAY_HELP ]]; then
         display_help
         exit 0
     fi
 }
 
-set_operating_system()
-{
+set_operating_system() {
     OS=$(uname -s)
 }
 
-configure_build_parallelism()
-{
+configure_build_parallelism() {
     if [[ $PARALLEL ]]; then
         display_message "Using shell-defined PARALLEL value."
     elif [[ $OS == Linux ]]; then
@@ -173,29 +155,26 @@ configure_build_parallelism()
     fi
 }
 
-setup_build_flags()
-{
+setup_build_flags() {
     ADDITIONAL_FLAGS="-Wall -fPIC"
     export CFLAGS="$ADDITIONAL_FLAGS $CFLAGS"
     export CXXFLAGS="$ADDITIONAL_FLAGS $CXXFLAGS"
 }
 
-setup_options()
-{
+setup_options() {
     CONFIGURE_OPTIONS=("$@" "--enable-static" "--disable-shared")
     CONFIGURE_OPTIONS=("${CONFIGURE_OPTIONS[@]/--build-*/}")
-    
-    CONFIGURE_OPTIONS=( "${CONFIGURE_OPTIONS[@]}" "$CUSTOM_CONFIGURE_OPTIONS")
-    
+
+    CONFIGURE_OPTIONS=("${CONFIGURE_OPTIONS[@]}" "$CUSTOM_CONFIGURE_OPTIONS")
+
     if [[ ! ($PREFIX) ]]; then
         PREFIX="$(dirname "$0")/result"
         PREFIX="/usr/local"
-        CONFIGURE_OPTIONS=( "${CONFIGURE_OPTIONS[@]}" "--prefix=$PREFIX")
+        CONFIGURE_OPTIONS=("${CONFIGURE_OPTIONS[@]}" "--prefix=$PREFIX")
     fi
 }
 
-set_pkgconfigdir()
-{
+set_pkgconfigdir() {
     # Set the prefix-based package config directory.
     PREFIX_PKG_CONFIG_DIR="$PREFIX/lib/pkgconfig"
 
@@ -206,8 +185,7 @@ set_pkgconfigdir()
     with_pkgconfigdir="--with-pkgconfigdir=$PREFIX_PKG_CONFIG_DIR"
 }
 
-set_with_boost_prefix()
-{
+set_with_boost_prefix() {
     # Boost has no pkg-config, m4 searches in the following order:
     # --with-boost=<path>, /usr, /usr/local, /opt, /opt/local, $BOOST_ROOT.
     # We use --with-boost to prioritize the --prefix path when we build it.
@@ -227,8 +205,7 @@ configure_build_parallelism
 setup_build_flags
 setup_options "$@"
 
-display_configuration()
-{
+display_configuration() {
     display_message "libbitcoin-system installer configuration."
     display_message "--------------------------------------------------------------------"
     display_message "OS                    : $OS"
@@ -245,54 +222,52 @@ display_configuration()
     display_message "--------------------------------------------------------------------"
 }
 
-
 # Define build options.
 #==============================================================================
 # Define icu options.
 #------------------------------------------------------------------------------
 ICU_OPTIONS=(
-"--enable-draft" \
-"--enable-tools" \
-"--disable-extras" \
-"--disable-icuio" \
-"--disable-layout" \
-"--disable-layoutex" \
-"--disable-tests" \
-"--disable-samples")
+    "--enable-draft"
+    "--enable-tools"
+    "--disable-extras"
+    "--disable-icuio"
+    "--disable-layout"
+    "--disable-layoutex"
+    "--disable-tests"
+    "--disable-samples")
 
 # Define boost options.
 #------------------------------------------------------------------------------
 BOOST_OPTIONS=(
-"--with-atomic" \
-"--with-chrono" \
-"--with-date_time" \
-"--with-filesystem" \
-"--with-iostreams" \
-"--with-locale" \
-"--with-log" \
-"--with-program_options" \
-"--with-regex" \
-"--with-system" \
-"--with-thread" \
-"--with-test")
+    "--with-atomic"
+    "--with-chrono"
+    "--with-date_time"
+    "--with-filesystem"
+    "--with-iostreams"
+    "--with-locale"
+    "--with-log"
+    "--with-program_options"
+    "--with-regex"
+    "--with-system"
+    "--with-thread"
+    "--with-test")
 
 # Define secp256k1 options.
 #------------------------------------------------------------------------------
 SECP256K1_OPTIONS=(
-"--disable-tests" \
-"--enable-experimental" \
-"--enable-module-recovery" \
-"--enable-module-schnorrsig")
+    "--disable-tests"
+    "--enable-experimental"
+    "--enable-module-recovery"
+    "--enable-module-schnorrsig")
 
 # Define bitcoin-system options.
 #------------------------------------------------------------------------------
-update_prefix_options()
-{
+update_prefix_options() {
     set_pkgconfigdir
     set_with_boost_prefix
 
     BITCOIN_SYSTEM_OPTIONS=(
-        "${with_boost}" \
+        "${with_boost}"
         "${with_pkgconfigdir}")
 }
 
@@ -300,15 +275,13 @@ update_prefix_options()
 #==============================================================================
 
 # Because boost doesn't support autoconfig and doesn't like empty settings.
-initialize_boost_configuration()
-{
+initialize_boost_configuration() {
     BOOST_CXXFLAGS="cxxflags=$CXXFLAGS"
     BOOST_LINKFLAGS="linkflags=$LDFLAGS"
 }
 
 # Because boost doesn't use autoconfig.
-build_from_tarball_boost()
-{
+build_from_tarball_boost() {
     local SAVE_IFS="$IFS"
     IFS=' '
 
@@ -334,7 +307,6 @@ build_from_tarball_boost()
     mkdir -p "$BUILD_DIR/$EXTRACT"
     tar --extract --file "dependencies/$ARCHIVE" "--$COMPRESSION" --strip-components=1 --directory="$BUILD_DIR/$EXTRACT"
 
-
     push_directory "$BUILD_DIR"
     push_directory "$EXTRACT"
 
@@ -358,7 +330,6 @@ build_from_tarball_boost()
 
     ./bootstrap.sh \
         "--prefix=$PREFIX"
-
 
     ./b2 install \
         "variant=release" \
@@ -385,8 +356,7 @@ build_from_tarball_boost()
 }
 
 # Standard build from github.
-build_from_github()
-{
+build_from_github() {
     push_directory "$BUILD_DIR"
 
     local ACCOUNT=$1
@@ -413,8 +383,7 @@ build_from_github()
 }
 
 # Standard build of current directory.
-build_from_local()
-{
+build_from_local() {
     local MESSAGE="$1"
     local JOBS=$2
     local OPTIONS=$3
@@ -434,15 +403,13 @@ build_from_local()
 
 # The master build function.
 #==============================================================================
-build_all()
-{
+build_all() {
     update_prefix_options
 
     build_from_tarball_boost "$BOOST_URL" "$BOOST_ARCHIVE" bzip2 . "$PARALLEL" "${BOOST_OPTIONS[@]}" "$BOOST_CUSTOM_OPTIONS"
     build_from_github libbitcoin secp256k1 version7 "$PARALLEL" "${SECP256K1_OPTIONS[@]}" "$@"
     build_from_local "Building local libbitcoin-system" "$PARALLEL" "${BITCOIN_SYSTEM_OPTIONS[@]}" "$@"
 }
-
 
 # Build the primary library and all dependencies.
 #==============================================================================
